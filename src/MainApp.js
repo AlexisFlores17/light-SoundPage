35bcc3c0d6d288d093f7828bba0eb697719841aa
Alex <alex.p.flores17@gmail.com>
2020-08-05 23:01:53 -0500

    codigo secciones

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,53 +1,13 @@
 import React from 'react'
-import{
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect 
- }from 'react-router-dom';
-import Home from './components/Home'
-import Light from './components/Light'
-import Sound from './components/Sound'
-import Disruptive from './components/Disruptive'
-import Lab from './components/Lab'
-import Showreel from './components/ShowReel'
-import { NavBar } from './components/NavBar';
-import { MenuTitulo } from './components/MenuTitulo';
 import './css/MainApp.scss'
+import { AppRouter } from './pages/AppRouter'
 
 
 export const MainApp = () => {
     return (
         <div>
-            <Router>
-                <div>
-                    <NavBar/>
-                    <div>
-                        <MenuTitulo/>
-                        <Switch>
-                            <Route exact path="/">
-                            <Home />
-                            </Route>
-                            <Route path="/Light">
-                            <Light />
-                            </Route>
-                            <Route path="/Sound">
-                            <Sound />
-                            </Route>
-                            <Route path="/Disruptive">
-                            <Disruptive />
-                            </Route>
-                            <Route path="/Lab">
-                            <Lab />
-                            </Route>
-                            <Route path="/Showreel">
-                            <Showreel />
-                            </Route>
-                        </Switch>
-                    </div>
-                </div>
-
-            </Router>
+            <AppRouter/>
+            
         </div>
     )
 }
