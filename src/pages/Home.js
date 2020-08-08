113ec0de7a8010f919c04b4d936f75bc69f39eb3
Alex <alex.p.flores17@gmail.com>
2020-08-08 01:07:53 -0500

    cambios extraños que hay en mi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,12 @@ import LSDbackground from '../videos/LSD1.1.mp4'
 export default function Home({handleExplore}) {
   return (
     <div className="container">
-      <video autoPlay muted loop className="myVideo" type="video/mp4">
-        <source src={LSDbackground} />
-      </video>
+      <div>
+        <video autoPlay muted loop className="myVideo" type="video/mp4">
+          <source src={LSDbackground} />
+        </video>
+      </div>
+
       <div className="wrapper">
         <h5>
           Retamos el sentido de lo establecido con propuestas innovadoras en ciencia, arte y tecnología
