6b4d8fae2ba93cba4d0c143bc4787dd06e1b5a36
lorenzoajt <lorenzoajt@gmail.com>
2020-08-11 03:18:00 -0500

    explore

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Explore() {
     return (
-        <div>
-            Explore
+        <div className="explore-container">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/Light">Light</Link>
+                  </li>
+                  <li>
+                    <Link  to="/Sound">& Sound</Link>
+                  </li>
+                  <li>
+                    <Link to="/Disruptive">Disruptive</Link>
+                  </li>
+                  <li>
+                    <Link  to="/Lab">Lab</Link>
+                  </li>
+
+                </ul>
+              </nav>
         </div>
     )
 }
