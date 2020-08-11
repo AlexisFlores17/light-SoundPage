089c6af5f4cb056388d905b3a9d56642af236cde
Alex <alex.p.flores17@gmail.com>
2020-08-11 02:32:27 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import arrow from '../images/arrow-right.svg'
-
-import desktopVideo from '../videos/LSD1.1.mp4'
-import mobileImage from '../images/lab.png'
-import { useWindowWidth } from '../hooks/useWindowWidth';
 import { Link } from "react-router-dom";
 
 export const Home = ({history}) => {
-  
-  const backgroundUrl = useWindowWidth() >= 650 ? desktopVideo : mobileImage;
   
   return (
     <div className="home-container">
