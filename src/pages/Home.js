c62e7b146d7c57d4353b3f89a098dc474ea45813
Alex <alex.p.flores17@gmail.com>
2020-08-11 01:24:16 -0500

    nueva estructura

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import arrow from '../images/arrow-right.svg'
-import Explore from '../components/Explore'
 import LSDbackground from '../videos/LSD1.1.mp4'
-export default function Home({handleMenu}) {
+import LSDbackgroundPhoto from '../images/lab.png'
+export default function Home() {
   return (
     <>
-    <video autoPlay muted loop className="myVideo" type="video/mp4">
+    <div>
+      <video autoPlay muted loop className="myVideo" type="video/mp4" poster={LSDbackgroundPhoto}>
         <source src={LSDbackground} />
       </video>
+    </div>
+ 
     <div className="container">
-      
-      <div className="wrapper">
-        <h5 className="animate__animated animate__fadeInUp"> 
-          Retamos el sentido de lo establecido con propuestas innovadoras en ciencia, arte y tecnología
-        </h5>
-        <div className="btn-row">
-          <button className="explore-button" onClick={handleMenu} >Explore
-            <div className="arrow-icon" >
-              <img src={arrow} alt="row"/>
-            </div>
-          </button>
-        </div>        
-      </div>      
+
     </div>    
     </>
   );
--- src/pages/MenuPrincipal.js
+++ src/pages/MenuPrincipal.js
@@ -1,79 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import{
-    BrowserRouter as Router,
-    Switch,
-    Route,
- }from 'react-router-dom';
-import Conocenos from './Conocenos'
-import ShowReel from './ShowReel'
-import Contacto from './Contacto'
-import Home from './Home'
-import Header from '../components/Header';
+import React from 'react';
+import Home from './Home';
+
 
 
 export const MenuPrincipal = () => {
-  // State of our Menu
-  const [state, setState] = useState({
-    initial: false,
-    clicked: null,
-    menuName: ""
-  });
-  // State of our button
-  const [disabled, setDisabled] = useState(false);
-
-
-  // Toggle menu
-  const handleMenu = () => {
-    disableMenu();
-    if (state.initial === false) {
-      setState({
-        initial: null,
-        clicked: true,
-        menuName: "Volver"
-      });
-    } else if (state.clicked === true) {
-      setState({
-        clicked: !state.clicked,
-        menuName: ""
-      });
-    } else if (state.clicked === false) {
-      setState({
-        clicked: !state.clicked,
-        menuName: "Volver"
-      });
-    }
-  };
-
-  //Determine if out menu button should be disabled
-  const disableMenu = () => {
-    setDisabled(!disabled);
-    setTimeout(() => {
-      setDisabled(false);
-    }, 1200);
-  };
-  const handleState = estado => {
-    console.log(estado)
-    setState(estado);
-  }
 
   return (    
-    <Router>
-      <div className="App">
-        <Header disabled={disabled} handleMenu={handleMenu} state={state} handleState={handleState}/>
-        <div className="container">
-          <div className="wrapper">
-            <div className="home">
-              <Switch>       
-                <Route exact path="/" component={() => <Home handleMenu={handleMenu}/>} />         
-                <Route exact path="/Conocenos" component={Conocenos} />
-                <Route exact path="/Showreel" component={ShowReel} />
-                <Route exact path="/Contacto" component={Contacto} />                
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
 }
 
