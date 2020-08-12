edcf6df516b04b5702835a1cfda4853ea7cfa393
Alex <alex.p.flores17@gmail.com>
2020-08-11 20:53:06 -0500

    nrl

--- src/pages/ShowReel.js
+++ src/pages/ShowReel.js
@@ -2,17 +2,21 @@ import React from "react";
 
 function Showreel() {
 
-return (    
+return (
+	<>    
 		<div>Showreel</div>
-	// <iframe 	 	 
-	//   title="vimeo-player" 
-	//   src="https://player.vimeo.com/video/189176993" 
-	//   style={{position: "fixed",
-	//           right: "0",
-	//           bottom: "0",
-	//           minWidth: "100%",
-	//           minHeight: "100%",}} 
-	//           width="100%" height="90%" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+		<iframe 	 	 
+		title="vimeo-player" 
+		src="https://player.vimeo.com/video/189176993" 
+		style={{position: "fixed",
+				right: "0",
+				bottom: "0",
+				minWidth: "100%",
+				minHeight: "100%",}} 
+				width="100%" height="90%" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen>
+				
+		</iframe>
+	</>
 	);  
 }
 
