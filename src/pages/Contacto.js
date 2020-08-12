71413e0bffe4b306e9a8bdc683739f6fb94c7157
Alex <alex.p.flores17@gmail.com>
2020-08-12 02:31:53 -0500

    contacto img

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,10 +1,16 @@
 import React from "react";
+import contactoImg from '../images/contacto.jpg';
 
 function Contacto() {
 
 return (    
-		  
+	<>
+		<div>
+			<img src ={contactoImg} alt='contacto' />
+		</div>		
+
 		<div>Contacto</div>
+	</>
 	)
 }
 
