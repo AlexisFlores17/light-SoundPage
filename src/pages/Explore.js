1c56e6e3597e62ca6062a0cd350e46fa2fb2bd7a
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-08-11 20:51:42 -0500

    down nav bar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -4,35 +4,38 @@ import {Link} from 'react-router-dom'
 
 export default function Explore() {
     return (
+      <>
         <div className="explore-container">
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/Light">Light</Link>
-                  </li>
-                  <li>
-                    <Link  to="/Sound">& Sound</Link>
-                  </li>
-                  <li>
-                    <Link to="/Disruptive">Disruptive</Link>
-                  </li>
-                  <li>
-                    <Link  to="/Lab">Lab</Link>
-                  </li>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/Light">Light</Link>
+              </li>
+              <li>
+                <Link  to="/Sound">& Sound</Link>
+              </li>
+              <li>
+                <Link to="/Disruptive">Disruptive</Link>
+              </li>
+              <li>
+                <Link  to="/Lab">Lab</Link>
+              </li>
 
-                </ul>
-              </nav>
-              <div className="down-navBar">              
-                <span>
-                  <Link to="/Conocenos" className="link">Conócenos</Link>
-                </span>
-                <span>
-                  <Link to="/Showreel" className="link">Showreel</Link>
-                </span>
-                <span>
-                  <Link to="/Contacto" className="link">Contacto</Link>
-                </span>                
-              </div>
+            </ul>
+          </nav>
+          <div className="down-navBar">              
+            <div className="col-sm-4 col-12">
+              <Link to="/Conocenos" className="link">Conócenos</Link>
+            </div>
+            <div className="col-sm-4 col-12">
+              <Link to="/Showreel" className="link">Showreel</Link>
+            </div>
+            <div className="col-sm-4 col-12">
+              <Link to="/Contacto" className="link">Contacto</Link>
+            </div>                
+          </div>
         </div>
+        
+      </>
     )
 }
