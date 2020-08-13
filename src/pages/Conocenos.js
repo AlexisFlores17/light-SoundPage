30493c6b7f14e03f4a4483d9e8ae983aa60fb56e
lorenzoajt <lorenzoajt@gmail.com>
2020-08-13 01:32:22 -0500

    descripciones iniciales

--- src/pages/Conocenos.js
+++ src/pages/Conocenos.js
@@ -6,8 +6,8 @@ import alex from '../images/alex.JPG'
 
 
 const team = [
-	{name: "Alexis Flores", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio nisi, lacinia sed viverra et, dictum vitae nunc.", imagen: alex},
-	{name: "Alexis Guerrero", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio nisi, lacinia sed viverra et, dictum vitae nunc.", imagen: destruido},
+	{name: "Alexis Flores", descripcion: "Director tecnológico, curador de memes, siempre buscando nuevas tecnologías(en Amazon). Se pasó el metal slug con una moneda", imagen: alex},
+	{name: "Alexis Guerrero", descripcion: "Director de interactividad, rockstar retirado, amante de los taquitos de tripita y una vez casi hizo un 300 en el boliche", imagen: destruido},
 	{name: "Diana Urquiza", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio nisi, lacinia sed viverra et, dictum vitae nunc.", imagen: "http://placekitten.com/g/300/300"},
 	{name: "Lorenzo Torres", descripcion: "Director creativo, gurú del front-end y K-pop connoisseur.", imagen: lorenzo},	
 	{name: "Mijael Gutiérrez", descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio nisi, lacinia sed viverra et, dictum vitae nunc.", imagen: "http://placekitten.com/g/300/300"},
