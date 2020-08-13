c6c7f02b15b933e71808fb622e0992e42ee21af1
Alex <alex.p.flores17@gmail.com>
2020-08-12 20:41:44 -0500

    Update MenuPrincipal.js

--- src/pages/MenuPrincipal.js
+++ src/pages/MenuPrincipal.js
@@ -1,16 +1,4 @@
-import React, {useState, useEffect} from 'react';
-import{
-    BrowserRouter as Router,
-    Switch,
-    Route,
- }from 'react-router-dom';
-import Conocenos from './Conocenos'
-import ShowReel from './ShowReel'
-import Contacto from './Contacto'
-import {Home} from './Home'
-import Header from '../components/Header';
-import Explore from './Explore'
-import Footer from '../components/Footer'
+import React from 'react';
 import { AppRoutes } from '../routes/AppRoutes';
 
 export const MenuPrincipal = () => {
