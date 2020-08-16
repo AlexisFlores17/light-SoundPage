fea07b46b37bc7af16356bc5a72db6c5850111a6
Alex <alex.p.flores17@gmail.com>
2020-08-16 02:43:44 -0500

    contacto again :v

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -14,22 +14,25 @@ return (
 			<div id='div-img-contacto'>
 				<img className='contacto-img img-fluid' src ={contactoImg} alt='contacto' />
 			</div>		
-			<div>
-				<div>
+			<div className='contacto-content'>
+				<div className='contacto-quote'>
 					"El arte desafía a la tecnología y la tecnología inspira al arte"
-				</div>
-				<div>
+					<br/>
 					-John Lasseter.
 				</div>
 
-				<div>
+				<div className='contacto-texto'>
 					Texto inspiracional pa que nos contacte la bandita alterada
 
 					Enim aliquip enim ut exercitation quis ullamco. Cupidatat magna fugiat id irure ullamco. Incididunt laborum fugiat elit cupidatat minim nostrud do veniam. Dolore occaecat excepteur minim sint Lorem aliquip aute anim dolor sit nostrud esse dolor. Est consequat laboris et sint cillum excepteur irure eu ad aute.
 				</div>
 
-				<div className="img-contacto ">
+				<div className="contacto-img ">
 					<img className="img-fluid rounded" src={imgContacto}/>
+				</div>
+
+				<div className='contacto-redes'>
+					
 				</div>
 			</div>
 		</div>	
